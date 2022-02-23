78ca42f1ecd25a2210c12dca0dccc4f0dc019f6b
Lawrence LCI <lcheunglci@gmail.com>
2022-02-22 23:57:10 -0800

    Add a datasource component

--- src/pages/ContainerPage.js
+++ src/pages/ContainerPage.js
@@ -4,33 +4,46 @@ import { UserLoader } from "../ContainerComponents/UserLoader";
 import { UserInfo } from "../ContainerComponents/UserInfo";
 import { ProductInfo } from "../ContainerComponents/ProductInfo";
 import { ResourceLoader } from "../ContainerComponents/ResourceLoader";
+import { DataSource } from "../ContainerComponents/DataSource";
+import axios from "axios";
 
-const ContainerPage = () => (
-    <>
-        <h1>Container examples</h1>
-        <h2>Current User Loader component</h2>
-        <CurrentUserLoader>
-            <UserInfo />
-        </CurrentUserLoader>
-        <h2>User Loader component</h2>
-        <UserLoader userId={123}>
-            <UserInfo />
-        </UserLoader>
-        <UserLoader userId={234}>
-            <UserInfo />
-        </UserLoader>
-        <UserLoader userId={345}>
-            <UserInfo />
-        </UserLoader>
-        <h2>Resource Loader component</h2>
-        <ResourceLoader resourceName="user" resourceUrl="/users/123">
-            <UserInfo />
-        </ResourceLoader>
-        <ResourceLoader resourceName="user" resourceUrl="/products/1234">
-            <ProductInfo />
-        </ResourceLoader>
+const ContainerPage = () => {
 
-    </>
-)
+    const getServerData = url => async () => {
+            const response = await axios.get(url);
+            return response.data;
+    }
 
+    return (
+        <>
+            <h1>Container examples</h1>
+            <h2>Current User Loader component</h2>
+            <CurrentUserLoader>
+                <UserInfo />
+            </CurrentUserLoader>
+            <h2>User Loader component</h2>
+            <UserLoader userId={123}>
+                <UserInfo />
+            </UserLoader>
+            <UserLoader userId={234}>
+                <UserInfo />
+            </UserLoader>
+            <UserLoader userId={345}>
+                <UserInfo />
+            </UserLoader>
+            <h2>Resource Loader component</h2>
+            <ResourceLoader resourceName="user" resourceUrl="/users/123">
+                <UserInfo />
+            </ResourceLoader>
+            <ResourceLoader resourceName="user" resourceUrl="/products/1234">
+                <ProductInfo />
+            </ResourceLoader>
+            <h2>Data Source component</h2>
+            <DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
+                <UserInfo />
+            </DataSource>
+
+        </>
+    )
+    }
 export default ContainerPage;
