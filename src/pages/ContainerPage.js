c3883bc896aefc20d946a8297de88238d9c0a369
Lawrence LCI <lcheunglci@gmail.com>
2022-02-22 23:41:19 -0800

    Add reusable userloader component and update the server to provide ids

--- src/pages/ContainerPage.js
+++ src/pages/ContainerPage.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { CurrentUserLoader } from "../ContainerComponents/CurrentUserLoader";
+import { UserLoader } from "../ContainerComponents/UserLoader";
 import { UserInfo } from "../ContainerComponents/UserInfo";
 
 const ContainerPage = () => (
     <>
         <h1>Container examples</h1>
+        <h2>Current User component</h2>
         <CurrentUserLoader>
             <UserInfo />
         </CurrentUserLoader>
+        <h2>User component</h2>
+        <UserLoader userId={123}>
+            <UserInfo />
+        </UserLoader>
+        <UserLoader userId={234}>
+            <UserInfo />
+        </UserLoader>
+        <UserLoader userId={345}>
+            <UserInfo />
+        </UserLoader>
 
     </>
 )
