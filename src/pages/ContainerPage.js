327679407dad51244709734daf5f5ffeb4ed69f0
Lawrence LCI <lcheunglci@gmail.com>
2022-02-22 23:49:21 -0800

    Add a resource component

--- src/pages/ContainerPage.js
+++ src/pages/ContainerPage.js
@@ -2,15 +2,17 @@ import React from "react";
 import { CurrentUserLoader } from "../ContainerComponents/CurrentUserLoader";
 import { UserLoader } from "../ContainerComponents/UserLoader";
 import { UserInfo } from "../ContainerComponents/UserInfo";
+import { ProductInfo } from "../ContainerComponents/ProductInfo";
+import { ResourceLoader } from "../ContainerComponents/ResourceLoader";
 
 const ContainerPage = () => (
     <>
         <h1>Container examples</h1>
-        <h2>Current User component</h2>
+        <h2>Current User Loader component</h2>
         <CurrentUserLoader>
             <UserInfo />
         </CurrentUserLoader>
-        <h2>User component</h2>
+        <h2>User Loader component</h2>
         <UserLoader userId={123}>
             <UserInfo />
         </UserLoader>
@@ -20,6 +22,13 @@ const ContainerPage = () => (
         <UserLoader userId={345}>
             <UserInfo />
         </UserLoader>
+        <h2>Resource Loader component</h2>
+        <ResourceLoader resourceName="user" resourceUrl="/users/123">
+            <UserInfo />
+        </ResourceLoader>
+        <ResourceLoader resourceName="user" resourceUrl="/products/1234">
+            <ProductInfo />
+        </ResourceLoader>
 
     </>
 )
