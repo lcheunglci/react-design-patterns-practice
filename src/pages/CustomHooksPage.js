f1f0a4976809563995c4e352dc5a1239347dfaa3
Lawrence LCI <lcheunglci@gmail.com>
2022-02-24 22:32:03 -0800

    Add useResource custom hook

--- src/pages/CustomHooksPage.js
+++ src/pages/CustomHooksPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { CurrentUserInfo } from "../CustomHookComponents/CurrentUserInfo";
 import { UserInfo } from "../CustomHookComponents/UserInfo";
+import { ResourceUserInfo } from "../CustomHookComponents/ResourceUserInfo";
+import { ResourceProductInfo } from "../CustomHookComponents/ResourceProductInfo"
 
 const CustomHooksPage = () => {
 
@@ -13,6 +15,9 @@ const CustomHooksPage = () => {
         <UserInfo userId="123" />
         <UserInfo userId="234" />
         <UserInfo userId="345" />
+        <h2>useResource custom hook</h2>
+        <ResourceUserInfo userId="234" />
+        <ResourceProductInfo productId="1234" />
         </>
     )
 }
