32952d1c823e8ada1c616d75dbf18c40c50fc5ee
Lawrence LCI <lcheunglci@gmail.com>
2022-02-24 22:22:51 -0800

    Add useUser custom hook

--- src/pages/CustomHooksPage.js
+++ src/pages/CustomHooksPage.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { CurrentUserInfo } from "../CustomHookComponents/CurrentUserInfo";
+import { UserInfo } from "../CustomHookComponents/UserInfo";
 
 const CustomHooksPage = () => {
 
     return (
         <>
         <h1>Custom Hook Page</h1>
-        <h2>CurrentUser custom hook</h2>
+        <h2>useCurrentUser custom hook</h2>
         <CurrentUserInfo />
+        <h2>useUser custom hook</h2>
+        <UserInfo userId="123" />
+        <UserInfo userId="234" />
+        <UserInfo userId="345" />
         </>
     )
 }
