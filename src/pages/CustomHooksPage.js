f8d99f43f39755b666b2ffa93ad5d7e018f02581
Lawrence LCI <lcheunglci@gmail.com>
2022-02-24 22:17:49 -0800

    Add useCurrentUser custom hook

--- src/pages/CustomHooksPage.js
+++ src/pages/CustomHooksPage.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { CurrentUserInfo } from "../CustomHookComponents/CurrentUserInfo";
 
-const CustomHooksPage = () => (
-    <>
-    <h1>Custom Hook Page</h1>
-    <p></p>
-    </>
-)
+const CustomHooksPage = () => {
+
+    return (
+        <>
+        <h1>Custom Hook Page</h1>
+        <h2>CurrentUser custom hook</h2>
+        <CurrentUserInfo />
+        </>
+    )
+}
 
 export default CustomHooksPage;
